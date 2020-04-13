a1ba92637ae85dea52d0b8e2b8550b0a25c27b9d
Lauren Hallau <lhallau@gmail.com>
2020-04-13 13:14:58 -0500

    html routes created for index, exercise, stats

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,22 @@
 var path = require("path");
 const app = require("express").Router();
 
-// html route for homepage /
+// html route for homepage "/"
 app.get("/", (req, res) => {
     console.log("getting homepage");
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
+});
+
+//html route for exercise "/exercise"
+app.get("/exercise", (req, res) => {
+    console.log("getting your exercises");
+    res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
+});
+
+// html route for stats "/stats"
+app.get("/stats", (req, res) => {
+    console.log("getting your stats");
+    res.sendFile(path.join(__dirname, "../public/html/stats.html"));
 });
 
 module.exports = app;
