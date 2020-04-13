5a23e7a174b7f9c13e5fb9f17d338c062077d357
Lauren Hallau <lhallau@gmail.com>
2020-04-13 14:17:49 -0500

    api get route for api/workout completed and tested in postman

--- models/workout.js
+++ models/workout.js
@@ -1,55 +1,55 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema (
-    {
-        day: {
-            type: Date,
-            default: Date.now
-        },
-        exercises: [
-            {
-                type: {
-                    type: String,
-                    trim: true,
-                    required: "Please enter an excerise"
-                },
-                name: {
-                    type: String,
-                    trim: true,
-                    required: "Please enter a name"
-                },
-                duration: {
-                    type: Number
-                },
-                weight: {
-                    type: Number
-                },
-                reps: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                },
-            }
-        ]
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-    {
-        // this includes any virtual properties when the data is requested
-        toJSON:{
-            virtuals:true
-        }
-    }
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Please enter an excerise",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Please enter a name",
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    // this includes any virtual properties when the data is requested
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
 
 // Adding a dynmaically-created property to the schema
-workoutSchema.virtual("totalDuration").get(function() {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,17 @@ const app = require("express").Router();
 const workout = require("../models/workout");
 
 // api GET route for workouts
+app.get("/api/workouts", function (req, res) {
+    console.log("is this working?!");
+    workout 
+    .find({})
+    .then(data => {
+        console.log(data);
+        res.json(data)
+    })
+    .catch(err => {
+        console.log("An error has occurred"),
+        err
+    });
+});
 module.exports = app;
