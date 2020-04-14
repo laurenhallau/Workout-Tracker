cbfb38acc772c6ba4735dc02f6fcc13efbe79d24
Lauren Hallau <lhallau@gmail.com>
2020-04-14 12:12:54 -0500

    POST route for creating new workout made and tested on postman

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,27 +3,40 @@ const workout = require("../models/workout");
 
 // api GET route for workouts
 app.get("/api/workouts", function (req, res) {
-    workout 
+  workout
     .find({})
-    .then(data => {
-        console.log(data);
-        res.json(data)
+    .then((data) => {
+      console.log(data);
+      res.json(data);
     })
-    .catch(err => {
-        console.log("An error has occurred"),
-        err
+    .catch((err) => {
+      console.log("An error has occurred"), err;
     });
 });
 
 // api GET route for stats on the dashboard
-app.get("/api/workouts/stats", function(req, res) {
-    workout
+app.get("/api/workouts/stats", function (req, res) {
+  workout
     .find({})
-    .then(function(data) {
-        res.json(data);
+    .then(function (data) {
+      res.json(data);
     })
-    .catch(err => {
-        res.json(err);
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// api POST route for new workout
+app.post("/api/workouts", function ({ body }, res) {
+  console.log("adding a new workout for you!");
+  workout
+    .create(body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
     });
 });
 module.exports = app;
